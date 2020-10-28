b31b720aa3e5404a095e9cd94735880645e36a38
Sevindzh <sevindzh.ismailova.a@gmail.com>
2020-10-28 15:01:52 +0300

    Лаб 2. Начало

--- ISA_ac1904/ISA_ac1904/main.cpp
+++ ISA_ac1904/ISA_ac1904/main.cpp
@@ -1,4 +1,4 @@
-#include <iostream>
+/* #include <iostream>
 using namespace std;
 
 int main()
@@ -8,4 +8,33 @@ int main()
 	int ch;
 	cin >> ch;
 	cout << 10 / ch << endl;
+} */
+
+#include <iostream>
+#include <fstream>
+using namespace std;
+int main(int argc, char* argv[])
+{
+    setlocale(LC_ALL, "ru");
+    if (argc > 1)
+    {
+        if (argc > 2)
+        {
+            cout << "Какое имя по счёту вы хотите приветствовать\n";
+            int n;
+            cin >> n;
+            cout << "Hello, " << argv[n];
+        }
+        else
+        {
+            cout << "Hello, " << argv[1];
+        }
+    }
+    else
+    {
+        ofstream fout;
+        fout.open("Error.txt");
+        fout << "Ошибка: Имя не задано";
+        fout.close();
+    }
 }
